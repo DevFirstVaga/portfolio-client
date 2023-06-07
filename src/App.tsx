4de7510ee3ae20cbc1af341057818f89d4efaa5d
Andrew Domingues <drewdomi@proton.me>
2023-06-06 21:40:45 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import Button from "./components/Button";
 import { ThemeProvider } from "styled-components";
+
 import { theme } from "./theme/theme";
+import Button from "./components/Button";
+import GlobalStyle from "./theme/global";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <h1>Hello, World!!</h1>
       <Button>Button</Button>
     </ThemeProvider>
